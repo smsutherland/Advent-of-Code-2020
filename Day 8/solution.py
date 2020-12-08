a1e8dbf8b2ea4f70f71abd7288b5b1960af8cfde
smsutherland <sagan@farthinghalearms.com>
2020-12-08 00:22:33 -0500

    Day 8

---
+++ Day 8/solution.py
@@ -0,0 +1,54 @@
+with open("input.txt") as input:
+    inputList = input.read().splitlines()
+
+
+lineNumber = 0
+acc = 0
+linesDone = []
+while lineNumber not in linesDone:
+    linesDone.append(lineNumber)
+    line = inputList[lineNumber]
+    op = line[:3]
+    num = int(line[4:])
+    if op == "acc":
+        acc += num
+        lineNumber += 1
+    if op == "jmp":
+        lineNumber += num
+    if op == "nop":
+        lineNumber += 1
+print(acc)
+
+def executeCode(code):
+    lineNumber = 0
+    acc = 0
+    linesDone = []
+    while lineNumber not in linesDone:
+        if lineNumber == len(inputList):
+            print(acc)
+            return True
+        linesDone.append(lineNumber)
+        line = code[lineNumber]
+        op = line[:3]
+        num = int(line[4:])
+        if op == "acc":
+            acc += num
+            lineNumber += 1
+        if op == "jmp":
+            lineNumber += num
+        if op == "nop":
+            lineNumber += 1
+    return False
+
+
+
+for i in range(len(inputList)):
+    line = inputList[i]
+    op = line[:3]
+    newInputList = [x for x in inputList]
+    if op == "nop":
+        newInputList[i] = "jmp " + line[4:]
+    if op == "jmp":
+        newInputList[i] = "nop " + line[4:]
+    if executeCode(newInputList):
+        break
